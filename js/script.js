b336312bab979499b37e9daa93a26ed9ac1d7430
arivazhagan1994 <154947395+arivazhagan1994@users.noreply.github.com>
2026-03-18 11:31:23 +0000

    Hero section updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-window.addEventListener("scroll", function(){
-    const header = this.document.getElementById("header");
-    if (this.window.scrollY>50){
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+
+    const header = document.getElementById("header");
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    // Scroll Effect
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
+
+    // Toggle Menu
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
+    // Close Menu on Click
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            navMenu.classList.remove("active");
+        });
+    });
+
 });
