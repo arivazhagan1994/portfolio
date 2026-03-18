51ad7de4093cdda81fb24163f12200b19c1a672c
arivazhagan1994 <154947395+arivazhagan1994@users.noreply.github.com>
2026-03-18 11:54:38 +0000

    hero role updated

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    // ===== SELECT ELEMENTS =====
     const header = document.getElementById("header");
     const menuToggle = document.getElementById("menu-toggle");
     const navMenu = document.getElementById("nav-menu");
     const navLinks = document.querySelectorAll(".nav-link");
+    const typingElement = document.getElementById("typing-text");
 
-    // Scroll Effect
+    // ===== SCROLL EFFECT =====
     window.addEventListener("scroll", function () {
         if (window.scrollY > 50) {
             header.classList.add("scrolled");
@@ -14,16 +16,49 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Toggle Menu
+    // ===== MOBILE MENU =====
     menuToggle.addEventListener("click", () => {
         navMenu.classList.toggle("active");
     });
 
-    // Close Menu on Click
+    // Close menu on link click
     navLinks.forEach(link => {
         link.addEventListener("click", () => {
             navMenu.classList.remove("active");
         });
     });
+
+    // ===== TYPING EFFECT =====
+    const roles = ["Data Science", "AI Developer", "Boiler Operator"];
+
+    let index = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function typeEffect() {
+
+        let currentWord = roles[index];
+
+        if (isDeleting) {
+            typingElement.textContent = currentWord.substring(0, charIndex--);
+        } else {
+            typingElement.textContent = currentWord.substring(0, charIndex++);
+        }
+
+        let speed = isDeleting ? 50 : 100;
+
+        if (!isDeleting && charIndex === currentWord.length+1) {
+            speed = 1500;
+            isDeleting = true;
+        } 
+        else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            index = (index + 1) % roles.length;
+        }
+
+        setTimeout(typeEffect, speed);
+    }
+
+    typeEffect();
 
 });
